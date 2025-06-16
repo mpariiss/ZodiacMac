e49478ca0766e14017caec023f0a327f5bf13818
mpariiss <miguelparis2005@gmail.com>
2025-06-16 16:13:56 +0200

    prueba

--- ZodiacMac/ViewCells/HoroscopeViewCell.swift
+++ ZodiacMac/ViewCells/HoroscopeViewCell.swift
@@ -29,7 +29,7 @@ class HoroscopeViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+        // Configure the view for the selected statefff
     }
 
 }
